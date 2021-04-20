f9596059b50daa60ff9fa608491496e37d9dc06d
katarzynaadamczyk <kasia.cholost@gmail.com>
2021-04-20 20:50:03 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+def encode_str(strng, shift):
+    # your code
+    pass
+
+def decode(arr):
+    #your code
+    pass
+
+def main():
+    dec = encode_str('I love you', 2)
+    print(dec)
+    print(decode(dec))
+
+if __name__ == '__main__':
+    main()
