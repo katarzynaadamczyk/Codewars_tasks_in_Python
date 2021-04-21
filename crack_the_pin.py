205308ad6ebe33d25ce0e10dd765089f94cfca19
katarzynaadamczyk <kasia.cholost@gmail.com>
2021-04-21 21:29:41 +0200

    New file - crack_the_pin

---
+++ crack_the_pin.py
@@ -0,0 +1,16 @@
+def crack(hash):
+  # G00D LUCK
+  pass
+
+def main():
+    out = crack('827ccb0eea8a706c4c34a16891f84e7b')
+    print(out) 
+    print('Should be 12345')
+    out = crack('86aa400b65433b608a9db30070ec60cd')
+    print(out) 
+    print('Should be 00078')
+
+
+
+if __name__ == '__main__':
+    main()
