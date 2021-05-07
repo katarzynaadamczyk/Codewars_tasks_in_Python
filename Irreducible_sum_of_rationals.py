40dc5e448acba977a69f5a2d2909eceba965fc8c
katarzynaadamczyk <kasia.cholost@gmail.com>
2021-05-07 14:40:47 +0200

    Done irreducible sum of rationals

--- Irreducible_sum_of_rationals.py
+++ Irreducible_sum_of_rationals.py
@@ -1,11 +1,28 @@
+import math
+
 def sum_fracts(lst):
-    # your code
+    if len(lst):
+        result = lst[0]
+        for i in range(1, len(lst)):
+            # add two rationals
+            com_div = math.gcd(result[1], lst[i][1])
+            result[0] = result[0] * lst[i][1] // com_div + lst[i][0] * result[1] // com_div
+            result[1] = result[1] * lst[i][1] // com_div
+        com_div = math.gcd(result[0], result[1])
+        result[0] //= com_div
+        if result[1] == com_div:
+            result = result[0]
+        else:
+            result[1] //= com_div
+        return result
     return None
   
 
   
 def main():
-    pass
+    print(f'Result for sum_fracts([[1, 2], [1, 3], [1, 4]]) equals {sum_fracts([[1, 2], [1, 3], [1, 4]])} while it should equal [13, 12]')
+    print(f'Result for sum_fracts([[1, 3], [5, 3]]) equals {sum_fracts([[1, 3], [5, 3]])} while it should equal 2')
+
 
 
 
