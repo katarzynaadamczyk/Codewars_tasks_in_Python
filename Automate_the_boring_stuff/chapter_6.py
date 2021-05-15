d83e8b1ff481dcf4507251f33683f50e5a0f7668
katarzynaadamczyk <kasia.cholost@gmail.com>
2021-05-15 21:30:44 +0200

    Done chapter 6

--- Automate_the_boring_stuff/chapter_6.py
+++ Automate_the_boring_stuff/chapter_6.py
@@ -1,5 +1,26 @@
+def printTable(table):
+    
+    # define the width of each column
+    colwidth = []
+    for i in range(len(table)):
+        tmp = 0
+        for text in table[i]:
+            if len(text) > tmp:
+                tmp = len(text)
+        colwidth.append(tmp)
+    
+    # print
+    for i in range(len(table[0])):
+        for j in range(len(table)):
+            print(table[j][i].rjust(colwidth[j]), end=' | ')
+        print()
+
+
 def main():
-    pass
+    table = [['jabłka', 'pomarańcze', 'wiśnie', 'banany'],
+             ['Alicja', 'Bob', 'Karol', 'Dawid'],
+             ['psy', 'koty', 'łosie', 'gęsi']]
+    printTable(table)
     
 
 if __name__ == '__main__':
