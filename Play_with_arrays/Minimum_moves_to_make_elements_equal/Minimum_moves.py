6d933ea3d87f0e96080a2d039277409b5c5ab211
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2021-07-19 09:09:02 +0200

    My best solution

--- Play_with_arrays/Minimum_moves_to_make_elements_equal/Minimum_moves.py
+++ Play_with_arrays/Minimum_moves_to_make_elements_equal/Minimum_moves.py
@@ -1,38 +1,24 @@
 class Solution:
-    # pierwsze rozwiazanie niedokonczone
-    def minMoves2(self, nums: List[int]) -> int:
+    def minMoves2(self, nums):
         sorted_nums = sorted(nums)
         if sorted_nums[0] == sorted_nums[-1]:
             return 0
         
-        num = sorted_nums[0]
-        
-        nums_count = {num: 1}
-
-        for i in range(1, len(nums)):
-            if num == sorted_nums[i]:
-                nums_count[num] += 1
-            else:
-                num = sorted_nums[i]
-                nums_count.setdefault(num, 1)
-        
-        # mamy posortowane numery w sorted_nums 
-        # mamy slownik zawierajacy wszystkie numery i ich ilosc
-
-        pass
-
-    # drugie rozwiazanie not working yet
-    def minMoves3(self, nums: List[int]) -> int:
-        nums_count = {}
-        for i in nums:
-            nums_count.setdefault(i, 0)
-            nums_count[i] += 1
-
-        nums_sum = sum(nums)
-        avg = nums_sum // len(nums)
+        median = sorted_nums[len(nums) // 2]
         result = 0
 
-        for key in nums_sum.keys():
-            result += abs(avg - key) * nums_count[key]
+        for i in sorted_nums:
+            result += abs(median - i)
 
-        return result           
+        return result  
+
+def main():
+    sol = Solution()
+    print(sol.minMoves2([1, 2, 3]))
+    print(sol.minMoves2([5, 50, 25]))
+    print(sol.minMoves2([60, 123, 58]))
+
+
+if __name__ == '__main__':
+    main()
+    
