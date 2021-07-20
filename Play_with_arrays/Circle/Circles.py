4a7ed9aeb72052c84654d5c57c4bee71ff8f1f2d
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2021-07-20 22:25:16 +0200

    Found a bug in recent solution, first attempt to fix

--- Play_with_arrays/Circle/Circles.py
+++ Play_with_arrays/Circle/Circles.py
@@ -1,22 +1,24 @@
 from typing import List
 
 def solution(nums: List[int]) -> int:
-    result1 = 0
-    result2 = 0
+    evens = []
     for i in range(-1, len(nums) - 1):
         if (nums[i] + nums[i+1]) % 2 == 0:
-            if i % 2 == 0:
-                result1 += 1
-            else: 
-                result2 += 1
-    return result1 if result1 > result2 else result2
+            evens.append(1)
+        else:
+            evens.append(0)
+    # teraz jeszcze przejsc i sprawdzic jak to zrobic
+    while True:
+
+        pass    
+    
 
 
 def main():
     print(f'Solution for [4,2,5,8,7,3,7] is {solution([4,2,5,8,7,3,7])}')
     print(f'Solution for [14,21,16,35,22] is {solution([14,21,16,35,22])}')
     print(f'Solution for [5,5,5,5,5,5] is {solution([5,5,5,5,5,5])}')
-    
+    print(f'Solution for [5,5,2,7,3,1,3,4,10,2] is {solution([5,5,2,7,3,1,3,4,10,2])} (it should equal 4')
 
 
 if __name__ == '__main__':
