f770dfb1219563fc31c0f16079a85ff3fb839371
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2021-09-26 22:43:05 +0200

    Added 2 files to tasks_about_strings

---
+++ Tasks_about_strings/rle.py
@@ -0,0 +1,15 @@
+def run_length_encoding(s):
+    return []
+    
+
+
+def main():
+    pass
+#    print(f'Solution for [4,2,5,8,7,3,7] is {solution([4,2,5,8,7,3,7])}')
+#    print(f'Solution for [14,21,16,35,22] is {solution([14,21,16,35,22])}')
+#    print(f'Solution for [5,5,5,5,5,5] is {solution([5,5,5,5,5,5])}')
+#    print(f'Solution for [5,5,2,7,3,1,3,4,10,2] is {solution([5,5,2,7,3,1,3,4,10,2])} (it should equal 4')
+
+
+if __name__ == '__main__':
+    main()
