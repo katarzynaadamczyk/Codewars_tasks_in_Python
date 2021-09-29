5d484420c754c0a42bb66b7353361850e70fd266
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2021-09-29 23:26:39 +0200

    Bug fixed in circles.py

--- Play_with_arrays/Circle/Circles.py
+++ Play_with_arrays/Circle/Circles.py
@@ -14,60 +14,33 @@ def solution(nums: List[int]) -> int:
         else:
             evens.append(0)
 
-    if count_of_evens == len(evens):
-        return len(evens) // 2
+    if count_of_evens >= len(evens) - 1:
+        return count_of_evens // 2
     if count_of_evens < 2:
         return count_of_evens
     
-    count_neighbours_1 = 0
-    count_neighbours_2 = 0
+    i, count_act, result = 0, 0, 0
 
-    for i in range(len(evens)):
-        if evens[i] == 1 and evens[i - 1] == 1:
-            count_neighbours_1 += 1
-            if evens[i-2] == 1:
-                count_neighbours_2 += 1    
+    while evens[i] == 1:
+        i -= 1
 
-    return count_of_evens - count_neighbours_1 + count_neighbours_2
+    for j in range(i, len(evens) + i):
+        if evens[j] == 1:
+            count_act += 1
+        else:
+            result += (count_act + 1) // 2
+            count_act = 0
 
-
-def solution_v2(nums: List[int]) -> int:
-
-    if len(nums) < 2:
-        return 0
-    if len(nums) == 2:
-        if nums[0] + nums[1] % 2 == 0:
-            return 1
-        return 0
-    
-
-    count_of_evens = 0
-    count_neighbours_1 = 0
-    count_neighbours_2 = 0
-
-    for i in range(-2, len(nums) - 2):
-        if (nums[i] + nums[i+1]) % 2 == 0:
-            count_of_evens += 1
-            if (nums[i-1] + nums[i]) % 2 == 0:
-                count_neighbours_1 += 1
-                if (nums[i+1] + nums[i+2]) % 2 == 0:
-                    count_neighbours_2 += 1
-
-    return count_of_evens - count_neighbours_1 + count_neighbours_2
-    
+    return result + (count_act + 1) // 2
     
 
 def main():
-    print('Solution v1')
     print(f'Solution for [4,2,5,8,7,3,7] is {solution([4,2,5,8,7,3,7])} (it should equal 2)')
     print(f'Solution for [14,21,16,35,22] is {solution([14,21,16,35,22])} (it should equal 1)')
     print(f'Solution for [5,5,5,5,5,5] is {solution([5,5,5,5,5,5])} (it should equal 3)')
     print(f'Solution for [5,5,2,7,3,1,3,4,10,2] is {solution([5,5,2,7,3,1,3,4,10,2])} (it should equal 4)')
-    print('Solution v2')
-    print(f'Solution for [4,2,5,8,7,3,7] is {solution([4,2,5,8,7,3,7])} (it should equal 2)')
-    print(f'Solution for [14,21,16,35,22] is {solution([14,21,16,35,22])} (it should equal 1)')
-    print(f'Solution for [5,5,5,5,5,5] is {solution([5,5,5,5,5,5])} (it should equal 3)')
-    print(f'Solution for [5,5,2,7,3,1,3,4,10,2] is {solution([5,5,2,7,3,1,3,4,10,2])} (it should equal 4)')
+    print(f'Solution for [5,5,5,5,0,5] is {solution([5,5,5,5,0,5])} (it should equal 2)')
+    print(f'Solution for [5,5,0,5,0,5] is {solution([5,5,0,5,0,5])} (it should equal 1)')
 
 
 if __name__ == '__main__':
