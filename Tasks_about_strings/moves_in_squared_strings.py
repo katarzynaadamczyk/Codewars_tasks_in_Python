6fc2cbe5ac9e39a1d56c94fe37ab3d7f9db4187b
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2021-11-01 23:11:06 +0100

    created moves_in_squared_strings, updated readme

---
+++ Tasks_about_strings/moves_in_squared_strings.py
@@ -0,0 +1,18 @@
+def rot(strng):
+    # your code
+    pass
+
+def selfie_and_rot(strng):
+    # your code
+    pass
+
+def oper(fct, s):
+    # your code
+    pass
+
+def main():
+    pass
+    
+
+if __name__ == '__main__':
+    main()
