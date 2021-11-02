2a0c7dd20f0a43127f6f2c9d73832cbbfded8020
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2021-11-02 09:32:05 +0100

    first attempt to rot

--- Tasks_about_strings/moves_in_squared_strings.py
+++ Tasks_about_strings/moves_in_squared_strings.py
@@ -1,6 +1,14 @@
 def rot(strng):
     # your code
-    pass
+    ln = strng.find('\n')
+    ret = ''
+    for i in range(ln):
+        for j in range(ln):
+            ret += strng[ln * (ln - i) - 1 - j] # + policzyc dokładnie co dodajemy
+        if i < (ln - 1):
+            ret += '\n'
+
+    return ret
 
 def selfie_and_rot(strng):
     # your code
@@ -11,6 +19,8 @@ def oper(fct, s):
     pass
 
 def main():
+    print('abcd\nefgh\nijkl\nmnop')
+    print(rot('abcd\nefgh\nijkl\nmnop'))
     pass
     
 
