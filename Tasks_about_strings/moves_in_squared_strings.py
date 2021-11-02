84fd916fb90e30c4b22b4db992d4c3b6184e7c95
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2021-11-02 21:03:25 +0100

    done selfie_and_rot, added tests

--- Tasks_about_strings/moves_in_squared_strings.py
+++ Tasks_about_strings/moves_in_squared_strings.py
@@ -1,5 +1,4 @@
 def rot(strng):
-    # your code
     ln = strng.find('\n')
     ret = ''
     for i in range(ln):
@@ -11,20 +10,26 @@ def rot(strng):
     return ret
 
 def selfie_and_rot(strng):
-    # your code
-    pass
+    ln = strng.find('\n')
+    ret = rot(strng)
+    for i in range(ln):
+        strng = strng[0:(ln * 2 + 1) * i + ln] + '.' * ln + strng[(ln * 2 + 1) * i + ln::]
+        ret = ret[0:(ln * 2 + 1) * i] + '.' * ln + ret[(ln * 2 + 1) * i::]
+    return strng + '\n' + ret
 
 def oper(fct, s):
     return fct(s)
 
 def main():
-    print('TESTING rot')
-    print('FIRST TEST \n' + rot('abcd\nefgh\nijkl\nmnop') +' should equal \n"ponm\nlkji\nhgfe\ndcba"')
-    print('SECOND TEST \n' + rot("fijuoo\nCqYVct\nDrPmMJ\nerfpBA\nkWjFUG\nCVUfyL") +' should equal \n"LyfUVC\nGUFjWk\nABpfre\nJMmPrD\ntcVYqC\nooujif"')
-    print('THIRD TEST \n' + rot("rkKv\ncofM\nzXkh\nflCB") +' should equal \n"BClf\nhkXz\nMfoc\nvKkr"')
+    print('TESTING rot\n')
+    print('FIRST TEST \n' + rot('abcd\nefgh\nijkl\nmnop') +'\nshould equal \nponm\nlkji\nhgfe\ndcba')
+    print('SECOND TEST \n' + rot("fijuoo\nCqYVct\nDrPmMJ\nerfpBA\nkWjFUG\nCVUfyL") +'\nshould equal \nLyfUVC\nGUFjWk\nABpfre\nJMmPrD\ntcVYqC\nooujif')
+    print('THIRD TEST \n' + rot("rkKv\ncofM\nzXkh\nflCB") +'\nshould equal \nBClf\nhkXz\nMfoc\nvKkr')
 
-    pass
-    
+    print('\nTESTING selfie_and_rot\n')
+    print('FIRST TEST \n' + selfie_and_rot('abcd\nefgh\nijkl\nmnop') +'\nshould equal \nabcd....\nefgh....\nijkl....\nmnop....\n....ponm\n....lkji\n....hgfe\n....dcba')
+    print('SECOND TEST \n' + selfie_and_rot("xZBV\njsbS\nJcpN\nfVnP") +'\nshould equal \nxZBV....\njsbS....\nJcpN....\nfVnP....\n....PnVf\n....NpcJ\n....Sbsj\n....VBZx')
+    print('THIRD TEST \n' + selfie_and_rot("uLcq\nJkuL\nYirX\nnwMB") +'\nshould equal \nuLcq....\nJkuL....\nYirX....\nnwMB....\n....BMwn\n....XriY\n....LukJ\n....qcLu')
 
 if __name__ == '__main__':
     main()
