e48da925aab949ee8e1ba2006a493d742cee696e
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2021-11-05 20:28:02 +0100

    first attempt to root_to_leaf, not working yet

--- Tasks_about_graphs/root_to_leaf_sum.py
+++ Tasks_about_graphs/root_to_leaf_sum.py
@@ -9,13 +9,32 @@ class TreeNode:
 
 
 class Solution:
-    def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        pass
+    @staticmethod
+    def rek(num, node, count):
+        print(f'num: {num}')
+        print(f'count: {count}')
+        print(f'node.val: {node.val}')
+        if node.right is None and node.left is None:
+            return num, node, count + num * 10 + node.val
+        if node.right is not None:
+            num, node2, count = Solution.rek(num * 10 + node.val, node.right, count)
+        if node.left is not None:
+            num, node2, count = Solution.rek(num, node.left, count)
+        return num // 10, node, count
+
+    @staticmethod
+    def sumNumbers(root: Optional[TreeNode]) -> int:
+        num, node2, count = Solution.rek(0, root, 0)
+        return count
 
 
 
 
 def main():
+    three = TreeNode(3)
+    two = TreeNode(2)
+    one = TreeNode(1, three)
+    print(Solution.sumNumbers(one))
     pass
     
 
