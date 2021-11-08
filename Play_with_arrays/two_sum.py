a1c094327df1071fa535e000dee59bcdaa62d39e
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2021-11-08 22:18:16 +0100

    created files and updated readme for two_sum

---
+++ Play_with_arrays/two_sum.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pass
+    
+def main():
+    pass
+    
+
+if __name__ == '__main__':
+    main()
+    
