e21871edb0b9b3d36ce7a97cd018a15d61c0931a
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2021-11-13 10:43:00 +0100

    Added working solution and some tests

--- Linked_lists/remove_elements.py
+++ Linked_lists/remove_elements.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import List, Optional
 
 # Definition for singly-linked list.
 class ListNode:
@@ -14,10 +14,43 @@ class Solution:
             del head
             return head2
 
+        while head.val == val:
+            head = removeHead(head)
         
+        pom = head
+        while pom:
+            if pom.next is not None and pom.next.val == val:
+                pom.next = removeHead(pom.next)
+            else:
+                pom = pom.next
+        
+        return head
+
+
+def create_linked_list(arr: Optional[List[int]]) -> Optional[ListNode]:
+    if not arr:
+        return None
+    head = None
+    for i in reversed(arr):
+        head = ListNode(i, head)
+    return head
+
+def print_linked_list(head: Optional[ListNode]):
+    print('[', end='')
+    while head and head.next:
+        print(str(head.val) + ', ', end='')
+        head = head.next
+    print(str(head.val) + ']')
 
 def main():
-    pass
+    head = create_linked_list([1, 2, 5, 6, 4, 7, 8])
+    print(head)
+    print_linked_list(head)
+    head2 = Solution.removeElements(head, 6)
+    print(head)
+    print(head2)
+    print_linked_list(head)
+    print_linked_list(head2)
     
 
 if __name__ == '__main__':
