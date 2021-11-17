3a88fee33a6a7681e191f44f4d5b8de504fecea2
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2021-11-17 21:23:34 +0100

    Added working second solution and time counter

--- Play_with_arrays/TheMillionthFibonacci.py
+++ Play_with_arrays/TheMillionthFibonacci.py
@@ -1,5 +1,23 @@
-# first solution - too long
+import time
 
+def measuretime(func):
+    def wrapper(*args, **kwargs):
+        starttime = time.perf_counter_ns()
+        ret = func(*args, **kwargs)
+        endtime = time.perf_counter_ns()
+        print(f'Time needed: {endtime - starttime} ns')
+        return ret
+    return wrapper
+
+def measuretime2(func, n):
+    starttime = time.perf_counter_ns()
+    ret = func(n)
+    endtime = time.perf_counter_ns()
+    print(f'Time needed: {endtime - starttime} ns')
+    return ret
+
+# first solution - too long
+@measuretime
 def fib(n):
     lst = []
     lst.append(0)
@@ -12,12 +30,27 @@ def fib(n):
         lst.insert(0, lst[1] - lst[0])
     return lst[0]
 
+# second solution - way too long
 def fib_v2(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    if n > 1:
+        return fib_v2(n - 1) + fib_v2(n - 2)
+    return fib_v2(n + 2) - fib_v2(n + 1)
+
+# third solution - hope to be good
+def fib_v3(n):
     pass
 
 
 def main():
-    pass
+    for n in range(-10, 31, 5):
+        result = fib(n)
+        print(f'First sol for {n} is {result}')
+        result = measuretime2(fib_v2, n)
+        print(f'Second sol for {n} is {result}')
     
 
 if __name__ == '__main__':
