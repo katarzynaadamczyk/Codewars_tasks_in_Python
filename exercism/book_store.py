86cc60750aa0d2be54b44ebf0d4b3fde31ad0c09
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-05-04 22:29:27 +0200

    done solution for book-store

--- exercism/book_store.py
+++ exercism/book_store.py
@@ -6,22 +6,39 @@ cost_of_a_book_dict = {
                         1: 800, 
                         2: 800 * 0.95, 
                         3: 800 * 0.9, 
-                        4: 800 + 0.8,
+                        4: 800 * 0.8,
                         5: 800 * 0.75
                       }
 
-def count_repetitions(basket):
+def count_repetitions(basket, books_count=5):
     basket_count_dict = {}
-    for book in basket:
-        basket_count_dict.setdefault(book, 0)
-        basket_count_dict[book] += 1
+    for book in range(1, books_count + 1):
+        basket_count_dict[book] = basket.count(book)
     return sorted(basket_count_dict.values())
+
+def count_groups(counted_books):
+    counted_groups, act_index = {}, 0
+    while sum(counted_books) > 0:
+        if counted_books[act_index] > 0:
+            counted_groups[len(counted_books) - act_index] = counted_books[act_index]
+            for index in range(act_index, len(counted_books)):
+                counted_books[index] -= counted_groups[len(counted_books) - act_index]
+        act_index += 1
+    return counted_groups
 
 
 def total(basket):
-    counted_book = count_repetitions(basket)
-    # TODO
-    pass
+    counted_books = count_repetitions(basket)
+    counted_groups = count_groups(counted_books)
+    result = 0
+    for key in sorted(counted_groups.keys(), reverse=True):
+        if key - 2 in counted_groups.keys() and key == 5:
+            count_of_mean_group = min(counted_groups[key], counted_groups[key-2])
+            result += cost_of_a_book_dict[key - 1] * (key - 1) * count_of_mean_group * 2
+            counted_groups[key] -= count_of_mean_group
+            counted_groups[key - 2] -= count_of_mean_group
+        result += cost_of_a_book_dict[key] * key * counted_groups[key]
+    return result
 
     
 
