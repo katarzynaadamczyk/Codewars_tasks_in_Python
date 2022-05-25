3129b69c7785ff673e944d76c2bb72dd59abacb6
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-05-25 22:49:46 +0200

    working on exercise complex numbers

---
+++ exercism/complex_numbers.py
@@ -0,0 +1,50 @@
+''' exercise complex numbers '''
+
+import math
+
+class ComplexNumber:
+    def __init__(self, real, imaginary=0):
+        self.real = real
+        self.imaginary = imaginary
+
+    def __eq__(self, other):
+        if isinstance(other, ComplexNumber):
+            return True if self.real == other.real and self.imaginary == other.imaginary else False
+        elif self.imaginary == 0 and self.real == other:
+            return True
+        return False
+    
+    def __add__(self, other):
+        if isinstance(other, ComplexNumber):
+            return ComplexNumber(self.real + other.real, self.imaginary + other.imaginary)
+        self.real += other
+        return self
+
+    def __mul__(self, other):
+        if isinstance(other, ComplexNumber):
+            return ComplexNumber(self.real * other.real - self.imaginary * other.imaginary, self.imaginary * other.real + self.real * other.imaginary)
+        self.real *= other
+        self.imaginary *= other 
+        return self
+        
+    def __sub__(self, other):
+        if isinstance(other, ComplexNumber):
+            return ComplexNumber(self.real - other.real, self.imaginary - other.imaginary)
+        self.real -= other
+        return self
+
+    def __truediv__(self, other):
+        factor = other.real ** 2 + other.imaginary ** 2
+        real = self.real * other.real + self.imaginary * other.imaginary
+        imaginary = self.imaginary * other.real - self.real * other.imaginary
+        return ComplexNumber(real / factor, imaginary / factor)
+
+    def __abs__(self):
+        return math.sqrt(self.real ** 2 + self.imaginary ** 2)
+
+    def conjugate(self):
+        return ComplexNumber(self.real, -1 * self.imaginary)
+
+    def exp(self):
+        constant = math.e ** self.real
+        return ComplexNumber(constant * math.cos(self.imaginary) + constant * math.sin(self.imaginary))
