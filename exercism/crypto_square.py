cdb24cc8b75f810c614364a29c1a91ced0562e1e
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-06-13 22:23:55 +0200

    done exercise crypto-square

--- exercism/crypto_square.py
+++ exercism/crypto_square.py
@@ -3,7 +3,9 @@
 from math import sqrt
 
 def get_c_r(message_length):
-    r = int(sqrt(message_length)) + 1
+    r = int(sqrt(message_length))
+    if r * (r + 1) < message_length:
+        r += 1
     c = r if r * r >= message_length else r + 1
     return c, r
 
@@ -15,8 +17,15 @@ def cipher_text(plain_text):
             new_text += char.lower()
     # count c and r
     c, r = get_c_r(len(new_text))
-    print(c, r)
-    pass
+    print(new_text)
+    print(c, r, len(new_text))
+    result = [new_text[r_index * c:(r_index + 1) * c].ljust(c) for r_index in range(r)]
+    new_result = ['' for _ in range(c)]
+    for verse in result:
+        for c_index in range(len(verse)):
+            new_result[c_index] += verse[c_index]
+    return ' '.join(new_result)
 
 if __name__ == '__main__':
     print(cipher_text("Chill out."))
+    print(cipher_text('If man was meant to stay on the ground, god would have given us roots.'))
