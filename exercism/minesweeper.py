eaf2c2add87eaf1388a11279d97b35cafa9c5c99
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-06-25 10:18:23 +0200

    done exercise minesweeper

--- exercism/minesweeper.py
+++ exercism/minesweeper.py
@@ -6,7 +6,7 @@ def check(minefield):
     if len(minefield) > 0:
         line_len = len(minefield[0])
         for line in minefield:
-            if len(line) != 0 or re.match(r'^[ *]', line):
+            if len(line) != line_len or not re.match(r'^[ *]*$', line):
                 return True
     return False
 
@@ -15,16 +15,14 @@ def annotate(minefield):
         raise ValueError("The board is invalid with current input.")
     result = []
     for x in range(len(minefield)):
-        tmp = []
+        tmp = ''
         for y in range(len(minefield[x])):
             if minefield[x][y] == '*':
-                tmp.append('*')
+                tmp += '*'
             else:
                 act_count = 0
-                for x2 in range(min(x-1, 0), max(x+2, len(minefield))):
-                    for y2 in range(min(y-1, 0), max(y+2, len(minefield[x]))):
-                        if minefield[x2][y2] == '*':
-                            act_count += 1
-                result.append(act_count if act_count > 0 else ' ')
-            result.append(tmp)
+                for x2 in range(max(x-1, 0), min(x+2, len(minefield))):
+                    act_count += minefield[x2][max(0, y-1):min(len(minefield[x2]), y+2)].count('*')
+                tmp += str(act_count) if act_count > 0 else ' '
+        result.append(tmp)
     return result
