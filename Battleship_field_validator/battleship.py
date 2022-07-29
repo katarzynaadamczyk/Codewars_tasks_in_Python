fba6c54af25fdb29415096c42c2e949cd604d6b6
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-07-29 20:53:19 +0200

    added files to battleship battlefield validator

---
+++ Battleship_field_validator/battleship.py
@@ -0,0 +1,26 @@
+'''
+My solution to task found on codewars: https://www.codewars.com/kata/52bb6539a4cf1b12d90005b7
+
+'''
+
+def validate_battlefield(field):
+    # write your magic here
+	return True
+
+def test1():
+    battlefield = [[1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+                 [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+                 [1, 0, 1, 0, 1, 1, 1, 0, 1, 0],
+                 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                 [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    print('test1')
+    print(validate_battlefield(battlefield), 'should be True')
+    
+if __name__ == '__main__':
+    test1()
+    
