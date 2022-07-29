324fc72e7b39e943005851b769811ce46c22665d
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-07-29 21:17:31 +0200

    created first function for validator

--- Battleship_field_validator/battleship.py
+++ Battleship_field_validator/battleship.py
@@ -3,9 +3,17 @@ My solution to task found on codewars: https://www.codewars.com/kata/52bb6539a4c
 
 '''
 
+def check_result(result_dict):
+    should_be_dict = {4: 1, 3: 2, 2: 3, 1: 4}
+    for key in result_dict.keys():
+        if key not in should_be_dict.keys() or result_dict[key] != should_be_dict[key]:
+            return False
+    return True if len(result_dict) == len(should_be_dict) else False
+
 def validate_battlefield(field):
+    ships_count_dict = {}
     # write your magic here
-	return True
+    return check_result(ships_count_dict)
 
 def test1():
     battlefield = [[1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
