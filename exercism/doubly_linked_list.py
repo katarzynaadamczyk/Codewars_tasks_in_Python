115e43209b2f4e2fbabf5fba8b67b6bc24a65223
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-09-08 22:26:11 +0200

    done doubly linked list exercise

--- exercism/doubly_linked_list.py
+++ exercism/doubly_linked_list.py
@@ -1,3 +1,5 @@
+''' exercise linked list  '''
+
 class Node:
     def __init__(self, value, succeeding=None, previous=None):
         self.__value__ = value
@@ -21,17 +23,47 @@ class Node:
 
 
 class LinkedList:
-    def __init__(self):
-        pass
+    def __init__(self, vals=[]):
+        self.__head__ = None
+        self.__tail__ = None
+        for val in vals:
+            self.push(val)
 
     def push(self, value):
-        pass
+        newNode = Node(value, None, self.__head__)
+        if self.__tail__ is None:
+            self.__tail__ = newNode
+        if self.__head__ is not None:
+            self.__head__.set_next(newNode)
+        self.__head__ = newNode
+        return value
 
     def pop(self):
-        pass
+        retVal = self.__head__.value()
+        self.__head__ = self.__head__.previous()
+        return retVal
 
     def shift(self):
-        pass
+        retVal = self.__tail__.value()
+        self.__tail__ = self.__tail__.next()
+        return retVal
 
     def unshift(self, value):
-        pass
+        newNode = Node(value, self.__tail__, None)
+        if self.__head__ is None:
+            self.__head__ = newNode
+        if self.__tail__ is not None:
+            self.__tail__.set_previous(newNode)
+        self.__tail__ = newNode
+        return value
+
+def tests():
+    lst = LinkedList()
+    lst.push(10)
+    lst.push(20)
+    print(lst.shift(), 'should be: ', 10)
+    print(lst.shift(), 'should be: ', 20)
+    
+if __name__ == '__main__':
+    tests()
+    
