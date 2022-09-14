79ccc37d2837b86a94c640a19d9274875c79d953
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-09-14 22:21:34 +0200

    started working on zebra puzzle

--- exercism/zebra_puzzle.py
+++ exercism/zebra_puzzle.py
@@ -1,5 +1,7 @@
 ''' zebra puzzle '''
 
+from copy import deepcopy
+
 nation, color, pet, bev, cigar = 'nationality', 'house color', 'animal', 'beverage', 'cigarette'
 
 easy_hints = [
@@ -34,20 +36,29 @@ def solve():
         4: {},
         5: {}
     }
-    
-    for key in sol_dict.keys():
-        for help_dict in easy_hints:
-            not_in_original_dict = True
-            for help_key in help_dict.keys():
-                if help_key in sol_dict[key].keys():
-                    not_in_original_dict = False
-                    break
-            if not_in_original_dict:
-                sol_dict[key].update(help_dict)
-        print(sol_dict)
+    def solve_r(sol, easy_num=0, advanced_num=0):
+        if type(sol) == type(True) or sum([len(sol[key]) for key in sol.keys()]) == 25:
+            return sol
+        for i in range(easy_num, len(easy_hints)):
+            for key in sol.keys():
+                not_in_original_sol = True
+                for hint_key in easy_hints[i].keys():
+                    if hint_key in sol[key].keys():
+                        not_in_original_sol = False
+                        break
+                if not_in_original_sol:
+                    new_sol = deepcopy(sol)
+                    
+                    print(new_sol)
+                    for hint_key in easy_hints[i].keys():
+                        new_sol[key][hint_key] = easy_hints[i][hint_key]
+                    solve_r(new_sol, easy_num + 1)
+                    print(new_sol)
+                    sol = new_sol
+                    
+        return sol
                 
-    print(sol_dict)
-    return sol_dict
+    return solve_r(sol_dict)
 
 def drinks_water():
     solution = solve()
