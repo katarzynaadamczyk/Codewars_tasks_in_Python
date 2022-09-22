1a7fd429ccf940c99160e4a8ba1308b3c151e494
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-09-22 23:10:36 +0200

    working on poker

--- exercism/poker.py
+++ exercism/poker.py
@@ -4,7 +4,18 @@
 types = ['S', 'D', 'H', 'C']
 cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
-
+    
+'''
+STRAIGHT_FLUSH = # 5 cards in sequence, same color
+FOUR_OF_A_KIND = # 4 cards with same rank, different color
+FULL_HOUSE = # 3 same rank and 2 second rank, more valuable are those which are 3
+FLUSH = # 5 cards of same color, not in sequence
+STRAIGHT = # 5 cards in sequence, not same color
+THREE_OF_A_KIND = # 3 cards of same value, not same color
+TWO_PAIR = # 2 cards of one rank, 2 of other rank and the kicker
+ONE_PAIR = # 2 cards of one rank, and the mix
+HIGH_CARD = # ranks higher the card
+''' 
 
 def score(hand):
     return 0
@@ -15,23 +26,15 @@ def best_hands(hands):
     if len(hands) == 1:
         return [hands[0]]
     scores = [score(hand) for hand in hands]
-    return [hands[scores.index(max(scores))]]zqq
+    max_score = max(scores)
+    return [hands[index] for index, value in enumerate(scores) if value == max_score]
+   
+
+def main():
+    print(best_hands(["4H 6H 7H 8H 5H", "5S 7S 8S 9S 6S"]))
+    print('should equal ["5S 7S 8S 9S 6S"]')
     
-'''
-YACHT = lambda dice: 50 if len(set(dice)) == 1 else 0
-ONES = lambda dice: sum([x for x in dice if x == 1])
-TWOS = lambda dice: sum([x for x in dice if x == 2])
-THREES = lambda dice: sum([x for x in dice if x == 3])
-FOURS = lambda dice: sum([x for x in dice if x == 4])
-FIVES = lambda dice: sum([x for x in dice if x == 5])
-SIXES = lambda dice: sum([x for x in dice if x == 6])
-FULL_HOUSE = lambda dice: sum(dice) if len(set(dice)) == 2 and dice.count(dice[0]) in (2, 3) else 0
-FOUR_OF_A_KIND = lambda dice: sum(set([4 * x for x in dice if dice.count(x) >= 4]))
-LITTLE_STRAIGHT = lambda dice: 30 if len(set(dice)) == 5 and max(dice) == 5 else 0
-BIG_STRAIGHT = lambda dice: 30 if len(set(dice)) == 5 and min(dice) == 2 else 0
-CHOICE = lambda dice: sum(dice)
-
-def score(dice, category):
-    return category(dice)
-
-'''
+    
+if __name__ == '__main__':
+    main()
+    
