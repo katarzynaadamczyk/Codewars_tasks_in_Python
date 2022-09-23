6dea757a9d36a5b03566a831fb30a5e8f826af3b
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-09-23 12:21:59 +0200

    working on poker

--- exercism/poker.py
+++ exercism/poker.py
@@ -1,8 +1,8 @@
 
 ''' exercise poker '''
 
-types = ['S', 'D', 'H', 'C']
-cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+colors = ['S', 'D', 'H', 'C']
+ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
     
 '''
@@ -16,8 +16,26 @@ TWO_PAIR = # 2 cards of one rank, 2 of other rank and the kicker
 ONE_PAIR = # 2 cards of one rank, and the mix
 HIGH_CARD = # ranks higher the card
 ''' 
+# numbers added to each type of hand
+SF, FK, FH, FL, ST, TK, TP, OP, HC = 10, 9, 8, 7, 6, 5, 4, 3, 2
+STRAIGHT_FLUSH = lambda ranks_dict, colors_dict: SF if len(colors_dict) == 1 and (max(ranks_dict.keys()) - min(ranks_dict.keys())) == 4 else 0
+FOUR_OF_A_KIND = lambda ranks_dict, colors_dict: FK if 4 in ranks_dict.values() else 0
+FULL_HOUSE = lambda ranks_dict, colors_dict: FH if len(ranks_dict) == 2 and 3 in ranks_dict.values() else 0
+FLUSH = lambda ranks_dict, colors_dict: FL if len(colors_dict) == 1 else 0
+STRAIGHT = lambda ranks_dict, colors_dict: ST if (max(ranks_dict.keys()) - min(ranks_dict.keys())) == 4 else 0
+THREE_OF_A_KIND = lambda ranks_dict, colors_dict: TK if len(ranks_dict) == 3 and 3 in ranks_dict.values() else 0
+TWO_PAIR = lambda ranks_dict, colors_dict: TP if len(ranks_dict) == 3 and 2 in ranks_dict.values() else 0
+ONE_PAIR = lambda ranks_dict, colors_dict: OP if len(ranks_dict) == 4 else 0
+HIGH_CARD = lambda ranks_dict, colors_dict: HC
+
+
 
 def score(hand):
+    hand_ranks, hand_colors = [ranks.index(card[:-1]) for card in hand], [card[-1] for card in hand]
+    ranks_dict = {x: hand_ranks.count(x) for x in hand_ranks}
+    colors_dict = {x: hand_colors.count(x) for x in hand_colors}
+    print(ranks_dict)
+    print(colors_dict)
     return 0
 
 def best_hands(hands):
@@ -25,7 +43,7 @@ def best_hands(hands):
         raise ValueError('wrong number of hands')
     if len(hands) == 1:
         return [hands[0]]
-    scores = [score(hand) for hand in hands]
+    scores = [score(hand.split()) for hand in hands]
     max_score = max(scores)
     return [hands[index] for index, value in enumerate(scores) if value == max_score]
    
