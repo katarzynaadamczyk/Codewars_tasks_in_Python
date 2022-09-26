e6659cf11c8cc1514eb9a41ea46a084f4dd71f41
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-09-26 22:31:58 +0200

    done rectangles

--- exercism/rectangles.py
+++ exercism/rectangles.py
@@ -1,16 +1,38 @@
 ''' exercise rectangles '''
 
 from itertools import combinations
+import re
 
-def check_if_down_corresponds(x, down_line):
-    for point in down_line:
-        pass
-    pass
+def check_if_down_corresponds(upper_point, down_line, strings):
+    for down_point in down_line:
+        if down_point[1] == upper_point[1]:
+            y_min = min(upper_point[0], down_point[0]) + 1
+            y_max = max(upper_point[0], down_point[0])
+            if len(re.findall(r'[^|+]', ''.join([strings[y][down_point[1]] for y in range(y_min, y_max)]))) == 0:
+                return True
+            break
+    return False
 
-def count_rectangles(upper_line, down_line):
+
+def check_x_line(point1, point2, strings):
+    x_min = min(point1[1], point2[1]) + 1
+    x_max = max(point1[1], point2[1])
+    if len(re.findall(r'[^-+]', strings[point1[0]][x_min:x_max])) == 0:
+        return True
+    return False
+
+def count_rectangles(upper_line, down_line, strings):
+    count_rect = 0
     for upper_index, upper_point in enumerate(upper_line):
-        pass
-    return 0
+        if check_if_down_corresponds(upper_point, down_line, strings):
+            for second_upper_point in upper_line[upper_index+1::]:
+                if check_x_line(upper_point, second_upper_point, strings):
+                    if check_if_down_corresponds(second_upper_point, down_line, strings) and \
+                       check_x_line((down_line[0][0], upper_point[1]), (down_line[0][0], second_upper_point[1]), strings):
+                        count_rect += 1
+                else:
+                    break
+    return count_rect
 
 
 def rectangles(strings):
@@ -24,7 +46,7 @@ def rectangles(strings):
             if len(new_points_line):
                 points.append(new_points_line)
     for upper_line, down_line in combinations(points, 2):
-        rect_count += count_rectangles(upper_line, down_line)
+        rect_count += count_rectangles(upper_line, down_line, strings)
     return rect_count
 
 
