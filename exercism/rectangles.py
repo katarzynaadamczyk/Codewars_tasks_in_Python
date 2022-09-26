5f5e670b1dfb891cb6497258de1bbf3cbf84f6bb
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-09-26 11:56:48 +0200

    started working on rectangles

--- exercism/rectangles.py
+++ exercism/rectangles.py
@@ -1,7 +1,30 @@
 ''' exercise rectangles '''
 
+from itertools import combinations
+
+def check_if_down_corresponds(x, down_line):
+    for point in down_line:
+        pass
+    pass
+
+def count_rectangles(upper_line, down_line):
+    for upper_index, upper_point in enumerate(upper_line):
+        pass
+    return 0
+
+
 def rectangles(strings):
-    rect_count = 0
+    rect_count, points = 0, []
+    for y, line in enumerate(strings):
+        if line.count('+') >= 2:
+            new_points_line = []
+            for x, char in enumerate(line):
+                if char == '+':
+                    new_points_line.append((y, x))
+            if len(new_points_line):
+                points.append(new_points_line)
+    for upper_line, down_line in combinations(points, 2):
+        rect_count += count_rectangles(upper_line, down_line)
     return rect_count
 
 
