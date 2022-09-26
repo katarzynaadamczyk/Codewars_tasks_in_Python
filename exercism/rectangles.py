7cdb6bc17c314da3133eb14c9b38411326197bba
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-09-26 11:10:42 +0200

    started working on rectangles

---
+++ exercism/rectangles.py
@@ -0,0 +1,20 @@
+''' exercise rectangles '''
+
+def rectangles(strings):
+    rect_count = 0
+    return rect_count
+
+
+def main():
+    test = ["+------+----+",
+            "|      |    |",
+            "+---+--+    |",
+            "|   |       |",
+            "+---+-------+"
+           ]
+    print('Rectangles for test is', rectangles(test), 'while it should equal 3')
+    
+
+if __name__ == "__main__":
+    main()
+    
