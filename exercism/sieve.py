98700a7a89e681ae2d5c2dbcc8ecce75dfade73f
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-09-27 10:32:52 +0200

    improved sieve solution

--- exercism/sieve.py
+++ exercism/sieve.py
@@ -1,14 +1,14 @@
 ''' sieve exercise '''
 
 def primes(limit):
-    lst_of_primes, non_primes = list(range(2, limit + 1)), set()
-    for number in lst_of_primes:
+    non_primes = set()
+    for number in range(2, limit + 1):
         if number not in non_primes:
             new_number = number + number
             while new_number <= limit:
                 non_primes.add(new_number)
                 new_number += number
-    return [number for number in lst_of_primes if number not in non_primes]
+    return [number for number in range(2, limit + 1) if number not in non_primes]
 
 def main():
     print(primes(10))
