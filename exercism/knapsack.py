d312be06847bd9b7f24f53a011d71a9fae09e49c
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-10-18 22:53:53 +0200

    added second solution to knapsack exercise

--- exercism/knapsack.py
+++ exercism/knapsack.py
@@ -1,12 +1,12 @@
 ''' exercise knapsack '''
 
-def maximum_value(maximum_weight, items):
+from itertools import combinations
+
+def maximum_value_v1(maximum_weight, items):
     calc_table = [[0 for _ in range(maximum_weight + 1)]]
     for y, item in enumerate(items):
         new_line = [0]
         for i in range(maximum_weight):
-          #  print(item)
-           # print(new_line)
             if item["weight"] <= i + 1:
                 new_line.append(max(calc_table[y][i+1], item["value"] + calc_table[y][i + 1 - item["weight"]]))
             else:
@@ -16,9 +16,16 @@ def maximum_value(maximum_weight, items):
         print(line)
     return calc_table[len(items)][maximum_weight]
 
+def maximum_value_v2(maximum_weight, items):
+    calc_table = [(0, 0)]
+    for i in range(1, len(items) + 1):
+        for combo in combinations(items, i):
+            calc_table.append((sum([x['weight'] for x in combo]), sum([x['value'] for x in combo])))
+    return max(filter(lambda x: x[0] <= maximum_weight, calc_table), key=lambda x: x[1])[1]
+
 
 def main():
-    print(maximum_value(10,
+    print(maximum_value_v2(10,
                 [
                     {"weight": 2, "value": 5},
                     {"weight": 2, "value": 5},
