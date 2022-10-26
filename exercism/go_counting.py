0315c8259736e651bb5567a3dfb48ce7cbcce712
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-10-26 13:42:23 +0200

    added solution to go counting

---
+++ exercism/go_counting.py
@@ -0,0 +1,83 @@
+''' exercise Go Counting '''
+
+import queue
+
+BLACK = 'B'
+WHITE = 'W'
+NONE = ''
+
+class Board:
+    """Count territories of each player in a Go game
+
+    Args:
+        board (list[str]): A two-dimensional Go board
+    """
+
+    def __init__(self, board):
+        self.board = board
+
+    def territory(self, x, y):
+        """Find the owner and the territories given a coordinate on
+           the board
+
+        Args:
+            x (int): Column on the board
+            y (int): Row on the board
+
+        Returns:
+            (str, set): A tuple, the first element being the owner
+                        of that area.  One of "W", "B", "".  The
+                        second being a set of coordinates, representing
+                        the owner's territories.
+        """
+        if not (0 <= y < len(self.board) and 0 <= x < len(self.board[y])):
+            raise ValueError("Invalid coordinate")
+        coordinates_to_be_checked, territory_set, boundaries = queue.SimpleQueue(), set(), set()
+        if self.board[y][x] == ' ':
+            coordinates_to_be_checked.put((x, y))
+        while not coordinates_to_be_checked.empty():
+            actual_coordinate = coordinates_to_be_checked.get()
+            territory_set.add(actual_coordinate)
+            for new_coordinate in self.where_to_go(actual_coordinate):
+                if 0 <= new_coordinate[1] < len(self.board) and 0 <= new_coordinate[0] < len(self.board[new_coordinate[1]]):
+                    if self.board[new_coordinate[1]][new_coordinate[0]] == ' ':
+                        if new_coordinate not in territory_set:
+                            coordinates_to_be_checked.put(new_coordinate)
+                    else:
+                        boundaries.add(self.board[new_coordinate[1]][new_coordinate[0]])
+        stone = NONE
+        if len(boundaries) == 1:
+            stone = list(boundaries)[0]
+        return stone, territory_set
+
+    def territories(self):
+        """Find the owners and the territories of the whole board
+
+        Args:
+            none
+
+        Returns:
+            dict(str, set): A dictionary whose key being the owner
+                        , i.e. "W", "B", "".  The value being a set
+                        of coordinates owned by the owner.
+        """
+        pass
+    
+    def where_to_go(self, coordinate):
+        yield (-1 + coordinate[0], coordinate[1])
+        yield (1 + coordinate[0], coordinate[1])
+        yield (coordinate[0], coordinate[1] - 1)
+        yield (coordinate[0], coordinate[1] + 1)
+
+
+def main():
+    board = Board(["  B  ", " B B ", "B W B", " W W ", "  W  "])
+    stone, territory = board.territory(x=0, y=1)
+    print(stone)
+    print(territory)
+    print(board.territories())
+        
+if __name__ == '__main__':
+    main()
+    
+    
