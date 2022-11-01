e7b5de53abf2430ceda2cb65e4a06d620cf46f8b
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-11-01 22:52:32 +0100

    working on word search

---
+++ exercism/word_search.py
@@ -0,0 +1,18 @@
+''' exercise word search '''
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+
+class WordSearch:
+    def __init__(self, puzzle):
+        pass
+
+    def search(self, word):
+        pass
