1750263d4b01753a99c47702d227c92e707f129e
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2022-11-02 23:30:27 +0100

    working on word guess

--- exercism/word_search.py
+++ exercism/word_search.py
@@ -15,6 +15,9 @@ class Point:
     def __sub__(self, other):
         return Point(self.x - other.x, self.y - other.y)
 
+    def __repr__(self) -> str:
+        return '(' + str(self.x) + ', ' + str(self.y) + ')'
+
 
 
 class WordSearch:
@@ -23,9 +26,9 @@ class WordSearch:
     VERTICAL = 1
     DIAGONAL_LEFT_TO_RIGHT = 2
     DIAGONAL_RIGHT_TO_LEFT = 3
-    point_changes = {HORIZONTAL: lambda x, y: Point(x, y),
-                     VERTICAL: lambda x, y: Point(y, x), 
-                     DIAGONAL_LEFT_TO_RIGHT: lambda x, y: Point(), # TODO
+    point_changes = {HORIZONTAL: lambda x, y, x_: Point(x + x_, y),
+                     VERTICAL: lambda x, y, x_: Point(y + x_, x), 
+                     DIAGONAL_LEFT_TO_RIGHT: lambda x, y, x_: Point(x + x_, y + x_), 
                      DIAGONAL_RIGHT_TO_LEFT: lambda x, y: Point()} # TODO
     
     def __init__(self, puzzle):
@@ -36,28 +39,34 @@ class WordSearch:
     def search(self, word):
         word_reversed = word[::-1]
         for type_ in WordSearch.point_changes.keys():
-            for y, line in self.generate_lines(type_):
-                x = line.find(word)
-                if x >= 0:
-                    return (WordSearch.point_changes[type_](x, y), WordSearch.point_changes[type_](x + len(word) - 1, y))
-                x = line.find(word_reversed)
-                if x >= 0:
-                    return (WordSearch.point_changes[type_](x + len(word) - 1, y), WordSearch.point_changes[type_](x, y))
+            for y, x, line in self.generate_lines(type_):
+                x_ = line.find(word)
+                if x_ >= 0:
+                    return (WordSearch.point_changes[type_](x, y, x_), WordSearch.point_changes[type_](x + len(word) - 1, y, x_))
+                x_ = line.find(word_reversed)
+                if x_ >= 0:
+                    return (WordSearch.point_changes[type_](x + len(word) - 1, y, x_), WordSearch.point_changes[type_](x, y, x_))
         return None
 
     def generate_lines(self, type_):
         if type_ == WordSearch.HORIZONTAL:
             for y, line in enumerate(self.puzzle):
-                yield y, line
+                yield y, 0, line
         if type_ == WordSearch.VERTICAL:
             for x in range(len(self.puzzle[0])):
-                yield x, ''.join([self.puzzle[y][x] for y in range(len(self.puzzle))])
+                yield x, 0, ''.join([self.puzzle[y][x] for y in range(len(self.puzzle))])
         if type_ == WordSearch.DIAGONAL_LEFT_TO_RIGHT:
-            # TODO
-            yield 'abc'
+            x, y = len(self.puzzle[0]) - 1, 0
+            while y < len(self.puzzle):
+                yield y, x, ''.join([self.puzzle[y_][x_] for x_, y_ in zip(range(x, len(self.puzzle[0])), range(y, len(self.puzzle)))])
+                x -= 1
+                if x < 0:
+                    x = 0
+                    y += 1
+                    
         if type_ == WordSearch.DIAGONAL_RIGHT_TO_LEFT:
             # TODO
-            yield 'abc'    
+            yield 0, 0, 'abc'    
     
     def check(self, puzzle):
         if len(puzzle) == 0:
@@ -67,4 +76,29 @@ class WordSearch:
             return False
         return True
     
+def main():
+    guess = WordSearch(['abc', 'xyz'])
+    for line in guess.generate_lines(WordSearch.DIAGONAL_LEFT_TO_RIGHT):
+        print(line)
+    puzzle = WordSearch(
+    [
+        "jefblpepre",
+        "camdcimgtc",
+        "oivokprjsm",
+        "pbwasqroua",
+        "rixilelhrs",
+        "wolcqlirpc",
+        "screeaumgr",
+        "alxhpburyi",
+        "jalaycalmp",
+        "clojurermt",
+    ]
+)
+    print(puzzle.search("clojure"), (Point(0, 9), Point(6, 9)))
+    print(puzzle.search("elixir"), (Point(5, 4), Point(0, 4)))
+    print(puzzle.search("ecmascript"), (Point(9, 0), Point(9, 9)))
+    print(puzzle.search("rust"), (Point(8, 4), Point(8, 1)))
+
+if __name__ == '__main__':
+    main()
     
