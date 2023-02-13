27eec5a4d5a36621c5c7894e38dcb193b49971eb
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2023-02-13 12:23:28 +0100

    done squareroot exercise

--- exercism/squareroot.py
+++ exercism/squareroot.py
@@ -1,4 +1,33 @@
 ''' exercise square root '''
 
 def square_root(number):
-    pass
+    if number < 0:
+        return -1
+    if number == 0:
+        return 0
+    min_val, max_val, act_val = 1, number, 1
+    i = 0
+    while not (act_val ** 2 == number or (act_val ** 2 < number and (act_val + 1) ** 2 > number)):
+        if act_val ** 2 < number:
+            min_val = act_val
+        else:
+            max_val = act_val
+        act_val = (min_val + max_val) // 2
+        i += 1
+        if i > 15:
+            break
+    print(i)
+    return act_val
+
+def main():
+    print('test for squareroot: ')
+    print('squareroot for 1: ', square_root(1))
+    print('squareroot for -1: ', square_root(-1))
+    print('squareroot for 10: ', square_root(10))
+    print('squareroot for 16: ', square_root(16))
+    print('squareroot for 25*25: ', square_root(25*25))
+
+
+if __name__ == '__main__':
+    main()
+    
