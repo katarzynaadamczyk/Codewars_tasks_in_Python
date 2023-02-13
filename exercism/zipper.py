fe410625875d1dd54302191de80e0bd5e3558371
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2023-02-13 12:41:19 +0100

    started working on zipper

--- exercism/zipper.py
+++ exercism/zipper.py
@@ -2,28 +2,41 @@
 
 
 class Zipper:
+    
+    def __init__(self, data, parent) -> None:
+        self.parent = parent
+        self.set_value(data.get('value', 0))
+        self.set_left(data.get('left', None))
+        self.set_right(data.get('right', None))
+    
     @staticmethod
     def from_tree(tree):
-        
-        return self
+        root = Zipper(tree, None)
+        return root
 
     def value(self):
-        pass
+        return self.value
 
-    def set_value(self):
-        pass
+    def set_value(self, number):
+        self.value = number
 
     def left(self):
-        pass
+        return self.left
 
-    def set_left(self):
-        pass
+    def set_left(self, data):
+        if data is not None:
+            self.left = Zipper(data, self)
+        else:
+            self.left = None
 
     def right(self):
-        pass
+        self.right
 
-    def set_right(self):
-        pass
+    def set_right(self, data):
+        if data is not None:
+            self.right = Zipper(data, self)
+        else:
+            self.right = None
 
     def up(self):
         pass
