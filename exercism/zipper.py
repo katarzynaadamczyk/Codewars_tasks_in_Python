0c1a544f28740fbe4ce8dde1b741d62a8c50be06
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2023-02-13 12:50:46 +0100

    working on zipper

--- exercism/zipper.py
+++ exercism/zipper.py
@@ -15,31 +15,64 @@ class Zipper:
         return root
 
     def value(self):
-        return self.value
+        return self.__value__
 
     def set_value(self, number):
-        self.value = number
+        self.__value__ = number
 
     def left(self):
-        return self.left
+        return self.__left__
 
     def set_left(self, data):
         if data is not None:
-            self.left = Zipper(data, self)
+            self.__left__ = Zipper(data, self)
         else:
-            self.left = None
+            self.__left__ = None
 
     def right(self):
-        self.right
+        self.__right__
 
     def set_right(self, data):
         if data is not None:
-            self.right = Zipper(data, self)
+            self.__right__ = Zipper(data, self)
         else:
-            self.right = None
+            self.__right__ = None
 
     def up(self):
-        pass
+        return self.parent
 
     def to_tree(self):
-        pass
+        self_tree = dict()
+        self_tree.setdefault('value', self.value())
+        if self.__left__ is None:
+            self_tree.setdefault('left', None)
+        else:
+            self_tree.setdefault('left', self.__left__.to_tree())
+        if self.__right__ is None:
+            self_tree.setdefault('right', None)
+        else:
+            self_tree.setdefault('right', self.__right__.to_tree())
+        return self_tree
+
+
+def main():
+    initial = {
+    "value": 1,
+    "left": {
+        "value": 2,
+        "left": None,
+        "right": {"value": 3, "left": None, "right": None},
+    },
+    "right": {"value": 4, "left": None, "right": None},
+    }
+
+    zipper = Zipper.from_tree(initial)
+    print(zipper.left())
+    print(zipper.left().right())
+    result = zipper.left().right().value()
+    print(result)
+    
+if __name__ == '__main__':
+    main()
+    
+    
