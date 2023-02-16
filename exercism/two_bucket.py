a143d4b224d37b49365377ac262f2f818ba5a790
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2023-02-16 21:16:11 +0100

    started working on two bucket

---
+++ exercism/two_bucket.py
@@ -0,0 +1,14 @@
+''' exercise two bucket '''
+
+
+def measure(bucket_one, bucket_two, goal, start_bucket):    
+    pass
+
+
+def main():
+    print(measure(3, 5, 1, "one"), 'should equal:', (4, "one", 5))
+    print(measure(1, 3, 3, "two"), 'should equal:', (1, "two", 0))
+
+if __name__ == '__main__':
+    main()
+    
