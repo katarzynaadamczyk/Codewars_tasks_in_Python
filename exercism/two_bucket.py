4302dd30b2f796817f00b756be2f5353628220e0
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2023-02-17 12:14:20 +0100

    working on two bucket - added error messages

--- exercism/two_bucket.py
+++ exercism/two_bucket.py
@@ -1,9 +1,19 @@
 ''' exercise two bucket '''
 
+from math import gcd
+
+class Bucket:
+    def __init__(self, name, capacity, val=0):
+        self.capacity = capacity
+        self.name = name
+        self.act_val = val
 
 def measure(bucket_one, bucket_two, goal, start_bucket):    
-    pass
-
+    if not (goal <= bucket_one or goal <= bucket_two):
+        raise ValueError('goal bigger than buckets')    
+    if goal % gcd(bucket_one, bucket_two) != 0:
+        raise ValueError('cannot reach target')
+    return (0, "one", 0)
 
 def main():
     print(measure(3, 5, 1, "one"), 'should equal:', (4, "one", 5))
