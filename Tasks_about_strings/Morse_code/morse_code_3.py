c49763fda067d8b046c731e32ab0829eaa4f3faa
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2023-03-25 22:03:20 +0100

    working on morse 3, need to implement k-means algorithm

--- Tasks_about_strings/Morse_code/morse_code_3.py
+++ Tasks_about_strings/Morse_code/morse_code_3.py
@@ -13,17 +13,35 @@ MORSE_CODE = {'.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.':
               '---...': ':', '-.-.-.': ';', '-...-': '=', '.-.-.': '+', '-....-': '-', 
               '..--.-': '_', '.-..-.': '"', '...-..-': '$', '.--.-.': '@', '...---...': 'SOS'}
 
+def get_one(one, step):
+    if one < step:
+        return '.'
+    else:
+        return '-'
+
 def decodeBitsAdvanced(bits):
     # ToDo: Accept 0's and 1's, return dots, dashes and spaces
     bits = bits.strip('0')
     ones_len, zeros_len = [len(x) for x in re.findall(r'[1]{1,}', bits)], [len(x) for x in re.findall(r'[0]{1,}', bits)]
-    print(ones_len)
-    print(zeros_len)
-    return bits.replace('111', '-').replace('000', ' ').replace('1', '.').replace('0', '')
+    if sum(ones_len + zeros_len) != len(bits):
+        raise ValueError('got not only ones and zeros')
+    step = 5
+    result = ''
+    for one, zero in zip(ones_len, zeros_len):
+        result += get_one(one, step)
+        if 5 <= zero <= 10:
+            result += ' '
+        elif zero > 10:
+            result += '   '
+    result += get_one(ones_len[-1], step)
+    return result
 
 def decodeMorse(morseCode):
     # ToDo: Accept dots, dashes and spaces, return human-readable message
-    return morseCode.replace('.', 'E').replace('-', 'T').replace(' ', '')
+    words, result = morseCode.split('   '), []
+    for word in words:
+        result.append(''.join([MORSE_CODE[char] for char in word.split()]))
+    return ' '.join(result)
 
 def main():
     print(decodeMorse(decodeBitsAdvanced('0000000011011010011100000110000001111110100111110011111100000000000111011111111011111011111000000101100011111100000111110011101100000100000')))
