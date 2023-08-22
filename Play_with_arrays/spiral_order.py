5f47f70924dd914de46673bbc4e26018667dfff5
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2023-08-22 22:58:18 +0200

    added new file for task spiral order

---
+++ Play_with_arrays/spiral_order.py
@@ -0,0 +1,13 @@
+'''
+
+my solution to task: https://leetcode.com/problems/spiral-matrix/
+
+'''
+
+from typing import List
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        pass
+    
+    
