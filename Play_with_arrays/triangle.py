55e5d6b3f0b596755984d2794e96b2a5ffee32ea
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2023-09-05 23:08:18 +0200

    working on triangle, wrong answer using queues

--- Play_with_arrays/triangle.py
+++ Play_with_arrays/triangle.py
@@ -3,26 +3,34 @@
 my solution to problem: https://leetcode.com/problems/triangle/
 
 '''
-
+from queue import PriorityQueue
 from typing import List
 
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        act_sums = [0]
-        for y, row in enumerate(triangle):
-            new_sums = []
-            for i, value in enumerate(act_sums):
-                new_sums.append(row[i] + value)
-                if i < y:
-                    new_sums.append(row[i + 1] + value)
-            act_sums = new_sums
-            print(act_sums)
-        return min(act_sums)
+        stack = PriorityQueue()
+        act_sum, act_y, triangle_len = triangle[0][0], 0, len(triangle)
+        stack.put((-1 * act_y, act_sum, 0))
+        while not stack.empty():
+            act_y, act_sum, act_x = stack.get()
+            act_y = - 1 * act_y
+            if act_y + 1 == triangle_len:
+                return act_sum
+            stack.put((-1 * (act_y + 1), act_sum + triangle[act_y + 1][act_x], act_x))
+            stack.put((-1 * (act_y + 1), act_sum + triangle[act_y + 1][act_x + 1], act_x + 1))
+        return 0
 
 def main():
     triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+    for row in triangle:
+        print(row)
     sol = Solution()
     print(sol.minimumTotal(triangle), 'should equal 11')
+    triangle = [[-1],[2,3],[1,-1,-3]]
+    for row in triangle:
+        print(row)
+    sol = Solution()
+    print(sol.minimumTotal(triangle), 'should equal -1')
     
 
 if __name__ == '__main__':
