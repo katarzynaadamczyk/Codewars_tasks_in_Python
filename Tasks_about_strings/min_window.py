a5b2615dd7213a4489d6b059771b7bca1d5abba7
Katarzyna <kasia.cholost@gmail.com>
2024-07-29 23:54:45 +0200

    working on leetcode

--- Tasks_about_strings/min_window.py
+++ Tasks_about_strings/min_window.py
@@ -5,6 +5,7 @@ https://leetcode.com/problems/minimum-window-substring/description/
 '''
 
 from collections import Counter
+from queue import PriorityQueue
 
 class Solution:
     def findMinMaxIndex(self, s: str, tCounter: Counter) -> tuple[int, int]:
@@ -20,6 +21,7 @@ class Solution:
                 return False
         return True
     
+    # rekurencja
     def moveCounter(self, s: str, minIndex: int, maxIndex: int, tLength: int, tCounter: Counter) -> str:
         windows = set()
         if maxIndex - minIndex >= tLength and self.checkCounters(Counter(s[minIndex + 1:maxIndex + 1]), tCounter):
@@ -30,6 +32,56 @@ class Solution:
             return s[minIndex:maxIndex+1]
         return min(windows, key=lambda x: len(x))
 
+    def moveCounterDynamic(self, s: str, minIndex: int, maxIndex: int, tLength: int, tCounter: Counter) -> str:
+        last_word = (minIndex, maxIndex)
+        words, used_words = set(), set()
+        words.add(last_word)
+        used_words.add(last_word)
+        while words:
+            new_words = set()
+            for word in words:
+                last_word = word
+                if last_word[1] - last_word[0] >= tLength:
+                    if self.checkCounters(Counter(s[last_word[0] + 1:last_word[1] + 1]), tCounter) and (last_word[0] + 1, last_word[1]) not in used_words:
+                        new_words.add((last_word[0] + 1, last_word[1]))
+                        used_words.add((last_word[0] + 1, last_word[1]))
+                    if self.checkCounters(Counter(s[last_word[0]:last_word[1]]), tCounter) and (last_word[0], last_word[1] - 1) not in used_words:
+                        new_words.add((last_word[0], last_word[1] - 1))
+                        used_words.add((last_word[0], last_word[1] - 1))
+            del words
+            words = new_words
+        print(len(used_words))
+        return s[last_word[0]:last_word[1]+1]
+    
+    def moveCounterQueue(self, s: str, minIndex: int, maxIndex: int, tLength: int, tCounter: Counter) -> str:
+        indexQueue = PriorityQueue()
+        indexQueue.put((maxIndex - minIndex, minIndex, maxIndex))
+        usedWords = set()
+        usedWords.add((minIndex, maxIndex))
+        minWord = (minIndex, maxIndex)
+        while not indexQueue.empty():
+            _, minIndex, maxIndex = indexQueue.get()
+            changeMade = False
+            if maxIndex - minIndex >= tLength:
+                if self.checkCounters(Counter(s[minIndex + 1:maxIndex + 1]), tCounter) and (minIndex + 1, maxIndex) not in usedWords:
+                    usedWords.add((minIndex + 1, maxIndex))
+                    indexQueue.put((maxIndex - 1 - minIndex, minIndex + 1, maxIndex))
+                    changeMade = True
+                if self.checkCounters(Counter(s[minIndex:maxIndex]), tCounter) and (minIndex, maxIndex - 1) not in usedWords:
+                    usedWords.add((minIndex, maxIndex - 1))
+                    indexQueue.put((maxIndex - 1 - minIndex, minIndex, maxIndex - 1))
+                    changeMade = True
+            if not changeMade:
+              #  print(minIndex, maxIndex)
+              #  print(minWord)
+                if (maxIndex - minIndex) < (minWord[1] - minWord[0]):
+                    minWord = (minIndex, maxIndex)
+                    print(s[minWord[0]:minWord[1]+1])
+              #  else:
+               #     return s[minWord[0]:minWord[1]+1]
+        print(len(usedWords))
+        return s[minWord[0]:minWord[1]+1]
+
 
     def minWindow(self, s: str, t: str) -> str:
         tCounter = Counter(t)
@@ -37,7 +89,9 @@ class Solution:
             return ''
         minIndex, maxIndex = self.findMinMaxIndex(s, tCounter)
         tLength = len(t)
-        return self.moveCounter(s, minIndex, maxIndex, tLength, tCounter)
+        return self.moveCounterQueue(s, minIndex, maxIndex, tLength, tCounter)
+      #  return self.moveCounterDynamic(s, minIndex, maxIndex, tLength, tCounter)
+       # return self.moveCounter(s, minIndex, maxIndex, tLength, tCounter)
 
 def main():
     sol = Solution()
@@ -53,6 +107,15 @@ def main():
     s = ""
     t = "a"
     print('\'' + sol.minWindow(s, t) + '\'', 'should equal \'\'')
+    # test 4
+    s = "qdsvbyuipqxnhkbgqdgozelvapgcainsofnrfjzvawihgmpwpwnqcylcnufqcsiqzwhhhjchfmqmagkrexigytklnrdslmkniuurdwzikrwlxhcbgkjegwsvnvpzhamrwgjzekjobizbreexqqewmwubtjadlowhwhiarurvcsyvwcunsylgwhisrivezrmvzwwsqppuhnreqmtkkgrjozbhjwlkpzgqwejotylamcgeqzobihmwinduloecqmtoqcejcpmqusukulncsbabodxbtbeloxzgbesdveupyocuzryutyxjdulzvpklokspqkslqodqfhlgajatkxfntqorhzcxlwmdigoyxtrcccidnlyxidnevdveczbpwpugyjhveyxhcfkpqipboehjhcombrdzhyghjncnnzwpggezrvcfzjqjngvoyyqhwwohlsvarrpzavatrcasnqbazyrzxhivfydsqasjtjiteloxposdhtfgswhrfpomnteftyonjyiojxnznfeubjctijmnyaanwgsphieqhpgsoutbbxycjaxrklekogakpsbwdimkxvelpyosvmxgnuxzgejwmjgbehxhpmtohzbyxqsvepbrmzsufcqrnwttfscxgxlpxnpufirjxtdjuvfzzvqprlizelwmkjchwtcdbvpbosminsjpncehnmgtzegknkrmdvrhrgihywsoobdedhltvtmxzuhmeaakysrpybyzxqnouqszzfswahtzbanidoubilsgoqfnjubdmvclaxkaedbfeppj"
+    t = "fjknwevk"
+    print('\'' + sol.minWindow(s, t) + '\'', 'should equal \'\'')
+    # test 5
+    s = "xeaifhaqslynbcwxncwgeezbrjorzyuwevejcecuscjvgfutkrcqxbromihlgcjnzpybwcxqeglknhgzyiqxljnyrvlazvnyklbgoywugjftrltrvlrgueeobsoandazqbigbgbhqgdjtycojtwfydtbvjekmejdirjlymvquybnyddjxaoxfkyatckijvlrnwcnjxfdxgtvjweiyvfdhefaipkrnviaunpfmukkcdhlcmwcjbgqhnsqfdhsasuwhjbtfmdhrluvzqykugcbtutyzdqcxkyevaxcodjhogdpwbzsjducxpdzsvbpizvfbtirwtzmzebyhcqqfmueczdwveofgjkhesbamaolgrlpvcfcqbhubmtythdzspizijbwlqjrjvgfznhprqmudfsyoxzimhhutjsebcykxgpywznnpbhuizuwythkbohwzzacbanyhewdfmsvpzryamuyhdkkurgvrjysjntqrrvxfnuvonvqbrqjvbvpucklligu"
+    t = "xbnpukocakzqzuhdlxoga"
+    print('\'' + sol.minWindow(s, t) + '\'', 'should equal \'\'')
+
 
 if __name__ == '__main__':
     main()
